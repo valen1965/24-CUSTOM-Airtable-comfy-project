bf7c581a8da0146dea05840c43ebeab65e605767
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2024-01-06 03:24:09 +0200

    Finalized Project

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Home from './HomePage';
+import About from './AboutPage';
+import Cart from './CartPage';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import Error from './ErrorPage';
+import Checkout from './CheckoutPage';
+import PrivateRoute from './PrivateRoute';
+import AuthWrapper from './AuthWrapper';
+export {
+  Home,
+  About,
+  Cart,
+  Products,
+  SingleProduct,
+  Error,
+  Checkout,
+  PrivateRoute,
+  AuthWrapper,
+};
